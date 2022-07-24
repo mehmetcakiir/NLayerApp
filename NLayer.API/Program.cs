c7bfaf111c2e8c20c4e1bfc5e9a4106281c5df21
infom <info.mehmetcakir@gmail.com>
2022-07-24 23:35:18 +0300

    FluentValidation

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -8,12 +8,14 @@ using System.Reflection;
 using NLayer.Core.Services;
 using NLayer.Service.Services;
 using NLayer.Service.Mapping;
+using FluentValidation.AspNetCore;
+using NLayer.Service.Validations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
